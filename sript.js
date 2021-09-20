f175a2d737be56ae59331aaed32767a5479aac27
KantaVD <89583709+KantaVD@users.noreply.github.com>
2021-09-20 12:34:41 +0200

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,18 @@
+console.log("Hello Winc Academy")
+let mijnNaam = 'Kanta'
+console.log(mijnNaam)
+
+let nummer = 4
+let string = "4"
+
+console.log(nummer + nummer)
+console.log(string + string)
+
+
+console.log(nummer - nummer)
+console.log(nummer * nummer)
+console.log(nummer / nummer)
+console.log(nummer % 40)
+
+let age = 40
+console.log(typeof age)
